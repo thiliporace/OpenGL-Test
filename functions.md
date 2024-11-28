baf225bc1538646c13088cb4fe100105eeebf975
Thiago Leandro Liporace <85422792+thiliporace@users.noreply.github.com>
2024-11-28 17:09:19 -0300

    terminar triangulo do 0

--- functions.md
+++ functions.md
@@ -9,3 +9,67 @@ the screen, while all the rendering commands draw to the back buffer. As soon as
 the rendering commands are finished we swap the back buffer to the front buffer so the
 image can be displayed without still being rendered to, removing all the aforementioned
 artifacts. <br>
+
+### glGenBuffers(1, &VBO) <br>
+Vertex buffer objects (VBO) store a large number of vertices in the GPU’s memory. 
+The advantage of using those buffer objects is that we can send large batches of data all at once to the graphics 
+card, and keep it there if there’s enough memory left, without having to send data one vertex at a time. 
+Sending data to the graphics card from the CPU is relatively slow, so wherever we can we try to 
+send as much data as possible at once. Once the data is in the graphics card’s memory the vertex shader 
+has almost instant access to the vertices making it extremely fast. <br>
+
+### glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW) <br>
+glBufferData is a function specifically targeted to copy user-defined data into the currently
+bound buffer. Its first argument is the type of the buffer we want to copy data into: the vertex buffer
+object currently bound to the GL_ARRAY_BUFFER target. The second argument specifies the size
+of the data (in bytes) we want to pass to the buffer; a simple sizeof of the vertex data suffices.
+The third parameter is the actual data we want to send.
+The fourth parameter specifies how we want the graphics card to manage the given data. This
+can take 3 forms: <br>
+• GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times. <br>
+• GL_STATIC_DRAW: the data is set only once and used many times. <br>
+• GL_DYNAMIC_DRAW: the data is changed a lot and used many times. <br>
+
+### shaderProgram = glCreateProgram() <br>
+A shader program object is the final linked version of multiple shaders combined. To use the recently
+compiled shaders we have to link them to a shader program object and then activate this shader
+program when rendering objects. The activated shader program’s shaders will be used when we
+issue render calls.
+When linking the shaders into a program it links the outputs of each shader to the inputs of the
+next shader. This is also where you’ll get linking errors if your outputs and inputs do not match. <br>
+
+### glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0) <br>
+• The first parameter specifies which vertex attribute we want to configure. Remember that we
+specified the location of the position vertex attribute in the vertex shader with layout
+(location = 0). This sets the location of the vertex attribute to 0 and since we want to
+pass data to this vertex attribute, we pass in 0. <br>
+• The next argument specifies the size of the vertex attribute. The vertex attribute is a vec3 so
+it is composed of 3 values. <br>
+• The third argument specifies the type of the data which is GL_FLOAT (a vec* in GLSL
+consists of floating point values). <br>
+• The next argument specifies if we want the data to be normalized. If we’re inputting integer
+data types (int, byte) and we’ve set this to GL_TRUE, the integer data is normalized to 0 (or
+-1 for signed data) and 1 when converted to float. This is not relevant for us so we’ll leave
+this at GL_FALSE. <br>
+• The fifth argument is known as the stride and tells us the space between consecutive vertex
+attributes. Since the next set of position data is located exactly 3 times the size of a float
+away we specify that value as the stride. Note that since we know that the array is tightly
+packed (there is no space between the next vertex attribute value) we could’ve also specified
+the stride as 0 to let OpenGL determine the stride (this only works when values are tightly
+packed). Whenever we have more vertex attributes we have to carefully define the spacing
+between each vertex attribute but we’ll get to see more examples of that later on. <br>
+• The last parameter is of type void* and thus requires that weird cast. This is the offset of
+where the position data begins in the buffer. Since the position data is at the start of the data
+array this value is just 0. We will explore this parameter in more detail later on. <br>
+
+### glGenVertexArrays(1, &VAO) <br>
+A vertex array object (also known as VAO) can be bound just like a vertex buffer object and any
+subsequent vertex attribute calls from that point on will be stored inside the VAO. This has the
+advantage that when configuring vertex attribute pointers you only have to make those calls once
+and whenever we want to draw the object, we can just bind the corresponding VAO. This makes
+switching between different vertex data and attribute configurations as easy as binding a different
+VAO. All the state we just set is stored inside the VAO. <br>
+A vertex array object stores the following:<br>
+• Calls to glEnableVertexAttribArray or glDisableVertexAttribArray.<br>
+• Vertex attribute configurations via glVertexAttribPointer.<br>
+• Vertex buffer objects associated with vertex attributes by calls to glVertexAttribPointer.<br>
---
+++ hello_triangle.cpp
@@ -0,0 +1,205 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+//Checa se a compilacao do glCompileShader teve sucesso
+void checkShaderErrors(unsigned int shader) {
+	int success;
+	char infoLog[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "ERROR::" << shader << "COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+}
+
+//Checa se a linkagem do shaderProgram deu certo
+void checkShaderProgramErrors(unsigned int shaderProgram) {
+	int success;
+	char infoLog[512];
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::" << shaderProgram << "COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+}
+
+//Precisamos escrever o vertex shader na linguagem de shader GLSL. Embaixo esta o codigo fonte de um vertex shader basico em GLSL
+//Especificamos a versao do OpenGL (3.3) e escolhemos o perfil core
+const char* vertexShaderSource = "#version 330 core\n"
+//Atributo do vertice, recebe um vec3 pra sua posicao como entrada
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+//gl_Position e o output do vertex shader
+" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+//Variavel de output que define a cor final
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"}\n\0";
+
+int main() {
+
+	//Coordenadas 3D normalizadas (range de -1 a 1) dos vertices do triangulo
+	float vertices[] = {
+		//Como o Z de todos os pontos e 0 parece que a imagem e 2D
+		-0.5f, -0.5f, 0.0f, //Ponta direita
+		0.5f, -0.5f, 0.0f, //Ponta esquerda
+		0.0f, 0.5f, 0.0f //Ponta de cima
+	};
+
+	//Inicializa GLFW
+	glfwInit();
+	//Configura o GLFW, primeiro argumento diz qual opcao pra configurar, segundo argumento e um INT que representa o valor da config
+	//Usamos pra definir a versao usada do OpenGL
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//Usar o core profile diz pro GLFW que vamos acessar apenas features atuais do OpenGL
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+
+	//Cria a janela com width e height colocados nos primeiros 2 args, com titulo no 3 arg
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+	//Retorna se a janela nao foi criada
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	//Diz pro GLFW fazer com que a janela seja o contexto principal na thread atual
+	glfwMakeContextCurrent(window);
+
+
+
+	//Precisamos inicializar o GLAD antes de chamar qualquer funcao OpenGL
+	//Pega as funcoes especificas do OS pra serem carregadas
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+
+	//Precisamos dizer pro OpenGL o tamanho da janela de renderizacao pra ele mostrar os dados e coordenadas em relacao a janela
+	//Note that processed coordinates in OpenGL are between -1 and 1 so we effectively map from the range(-1 to 1) to (0, 800) and (0, 800).
+	glViewport(0, 0, 800, 800);
+
+	//Toda vez que o usuario redimensionar a tela o viewport tambem vai ser ajustado por essa funcao
+	void framebuffer_size_callback(GLFWwindow * window, int width, int height);
+
+	//Precisamos dizer pro GLFW chamar essa funcao quando a janela for redimensionada
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+
+	//Criando o vertex buffer object e o vertex array object
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+
+	//Faz o VAO o Vertex Array Object atual bindando ele
+	glBindVertexArray(VAO);
+
+	//Bindamos o VBO no tipo de buffer object GL_ARRAY_BUFFER que e pro VBO, agora qualquer mudanca feita no GL_ARRAY_BUFFER vai ser feita no VBO conectado
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	//Copia o dado dos vertices pra memoria do buffer
+	//1 arg: tipo de buffer que vamos copiar os dados para. 2 arg: tamanho dos dados que vai ser passado. 3 arg: dados. 4 arg: functions.md explica
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//Dizemos pro OpenGL como ele deve interpretar os dados do vertice por vertice, mais explicacao no functions.md
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	//Ligamos o vertex attribute na posicao 0, que foi onde criamos la no codigo fonte 
+	glEnableVertexAttribArray(0);
+
+
+	//Criando o vertex shader object
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	//Conectamos o codigo fonte do vertex shader no vertex shader object e compilamos o shader
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	checkShaderErrors(vertexShader);
+
+
+	//Criando o fragment shader object
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	//Conectamos o codigo fonte do fragment shader no fragment shader object e compilamos o shader
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	checkShaderErrors(fragmentShader);
+
+
+
+	//Precisamos criar o shader program que vai conectar todos os shaders que criamos, mais explicacao no functions.md
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	//Conectar os shaders criados no shader program e depois linka eles
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	checkShaderProgramErrors(shaderProgram);
+
+	//Nao precisamos mais dos shader objects
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+
+	//Nao queremos que a aplicacao rode a imagem e feche na hora, entao vamos criar um render loop, que roda ate que o GLFW seja dito pra parar
+	while (!glfwWindowShouldClose(window)) {
+
+		//INPUT
+		//Se detectar a tecla ESC, fecha a janela
+		processInput(window);
+
+		//RENDERIZACAO
+		//Limpa a tela com uma cor de nossa escolha
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Ativar o shader program
+		glUseProgram(shaderProgram);
+
+		//Conecta o VAO pro OpenGL saber como usar ele
+		glBindVertexArray(VAO);
+
+		//Desenha as primitivas com base no shader ativo
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		//EVENTOS
+		//Mais explicacao no functions.md
+		//Troca o back buffer pelo front buffer
+		glfwSwapBuffers(window);
+
+		//Checa se eventos foram acionados (input de teclado ou mouse) e atualiza a janela
+		glfwPollEvents();
+	}
+
+	//Ao sair do render loop precisamos limpar todos os recursos GLFW que foram alocados
+	glfwTerminate();
+	return 0;
+}
